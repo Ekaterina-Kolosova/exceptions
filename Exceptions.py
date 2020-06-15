ad78cd7fa606a29cdb36da501f9726fc42ae802e
Ekaterina-Kolosova <kolosova411@yandex.ru>
2020-06-15 21:52:01 +0300

    "Tasks divided in files, corrections made"

--- Exceptions.py
+++ Exceptions.py
@@ -1,91 +1,29 @@
+task = input('Введите через пробел действие и 2 числа')
+task_list = task.split(' ')
+operators = ['+', '-', '*', '/']
+
+assert task_list[0] in operators, 'Введено неверное арифметическое действие'
+assert task_list[2] in task_list, 'Введено менее двух чисел'
+
 try:
-    operator = input('Введите арифметическое действие')
-    operand1 = int(input('Введите первое число'))
-    operand2 = int(input('Введите второе число'))
-    operators = ['+', '-', '*', '/']
+    task_list[1] = int(task_list[1])
+    task_list[2] = int(task_list[2])
+except ValueError:
+    print('Введите числа')
 
-    if operator == "*":
-        result = operand1 * operand2
-    elif operator == "/":
-        result = operand1 / operand2
-    elif operator == "+":
-        result = operand1 + operand2
-    elif operator == "-":
-        result = operand1 - operand2
-
-    assert operator in operators, 'Введено неверное арифметическое действие'
-
-    print(f'результат: {result}')
-except Exception as e:
-    print(e)
-
-documents = [
-    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-    {"type": "passport", "number": "5455 028765"}
-]
-
-directories = {
-    '1': ['2207 876234', '11-2', '5455 028765'],
-    '2': ['10006'],
-    '3': []
-}
-
-
-def get_owner_name():
+if task_list[0] == "*":
+    result = task_list[1] * task_list[2]
+elif task_list[0] == "/":
     try:
-        document_input = input('Введите номер документа: ')
-        for document in documents:
-            if document_input == document["number"]:
-                print(f'Владелец документа: {document["name"]}')
-                break
-        else:
-            print('Документа с таким номером не существует')
-    except KeyError:
-        print('Не указано имя владельца')
+        task_list[2] != 0
+        result = task_list[1] / task_list[2]
+    except ZeroDivisionError:
+        print('На ноль делить нельзя')
+elif task_list[0] == "+":
+    result = task_list[1] + task_list[2]
+elif task_list[0] == "-":
+    result = task_list[1] - task_list[2]
 
+print(f'результат: {result}')
 
-def get_shelf_number():
-    document_input = input('Введите номер документа: ')
-    for document in directories.items():
-        if document_input in document[1]:
-            print(f'Номер полки: {document[0]}')
-            break
-    else:
-        print("Нет документа с таким номером")
-
-
-def get_list_all_document():
-    for document in documents:
-        print(f'{document["type"]} "{document["number"]}" "{document["name"]}"')
-
-
-def add_new_document(folder, directory):
-    document_number_input = input('Введите номер документа: ')
-    document_type_input = input('Введите тип документа: ')
-    document_owner_input = input('Введите имя владельца: ')
-    document_shelf_input = input('Введите номер полки: ')
-    folder.append({"type": document_type_input, "number": document_number_input, "name": document_owner_input})
-    if document_shelf_input in directory.keys():
-        directory[document_shelf_input].append(document_number_input)
-    else:
-        directory.setdefault(document_shelf_input, [document_number_input])
-    return folder, directory
-
-
-
-def main():
-    while True:
-        user_input = input('Введите команду: ')
-        if user_input == 'p':
-            get_owner_name()
-        elif user_input == 's':
-            get_shelf_number()
-        elif user_input == 'l':
-            get_list_all_document()
-        elif user_input == 'a':
-            add_new_document(documents, directories)
-
-main()
 
---
+++ Task_3.py
@@ -0,0 +1,68 @@
+
+documents = [
+    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
+    {"type": "passport", "number": "5455 028765"}
+]
+
+directories = {
+    '1': ['2207 876234', '11-2', '5455 028765'],
+    '2': ['10006'],
+    '3': []
+}
+def get_owner_name():
+  document_input = input('Введите номер документа: ')
+  for document in documents:
+    try:
+        if document_input == document["number"]:
+            print(f'Владелец документа: {document["name"]}')
+            break
+    except KeyError:
+        print('Не указано имя владельца')
+        break
+  else:
+    print('Документа с таким номером не существует')
+
+def get_shelf_number():
+    document_input = input('Введите номер документа: ')
+    for document in directories.items():
+        if document_input in document[1]:
+            print(f'Номер полки: {document[0]}')
+            break
+    else:
+        print("Нет документа с таким номером")
+
+
+def get_list_all_document():
+    for document in documents:
+        print(f'{document["type"]} "{document["number"]}" "{document["name"]}"')
+
+
+def add_new_document(folder, directory):
+    document_number_input = input('Введите номер документа: ')
+    document_type_input = input('Введите тип документа: ')
+    document_owner_input = input('Введите имя владельца: ')
+    document_shelf_input = input('Введите номер полки: ')
+    folder.append({"type": document_type_input, "number": document_number_input, "name": document_owner_input})
+    if document_shelf_input in directory.keys():
+        directory[document_shelf_input].append(document_number_input)
+    else:
+        directory.setdefault(document_shelf_input, [document_number_input])
+    return folder, directory
+
+
+
+def main():
+    while True:
+        user_input = input('Введите команду: ')
+        if user_input == 'p':
+            get_owner_name()
+        elif user_input == 's':
+            get_shelf_number()
+        elif user_input == 'l':
+            get_list_all_document()
+        elif user_input == 'a':
+            add_new_document(documents, directories)
+
+main()
